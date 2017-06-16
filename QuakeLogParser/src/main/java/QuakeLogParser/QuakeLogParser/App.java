ec406315532d4a61d688fa57a77702ca7c16632a
danisoares <danielespassos@gmail.com>
2017-06-16 16:10:56 -0300

    new basic project

---
+++ QuakeLogParser/src/main/java/QuakeLogParser/QuakeLogParser/App.java
@@ -0,0 +1,13 @@
+package QuakeLogParser.QuakeLogParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
